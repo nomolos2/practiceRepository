44086b6627dc6bcc14e5c680a9bca44998d865c5
Solomon Gold <solomon@sig2014.lan>
2019-10-05 16:05:07 -0400

    I added a image and some links and stuff

---
+++ test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function dostuff() {
+	let a = document.getElementById('target')  
+	let d = document.getElementById('get the duck')
+	let b  = prompt(a.textContent)
+	let e = prompt(d.textContent)
+	a.innerHTML = "fadfasdfaf"
+	let h2 = document.createElement('h2')
+	h2.innerHTML = "this is the added h2"
+	a.appendChild(h2)
+	let c  = prompt("Guys")
+	d.innerHTML = c 
+}
